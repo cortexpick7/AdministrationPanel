823bedeaedba7aa3f03fd876e4ad33e4d016a184
cortexpick7 <artkhokhlov19@gmail.com>
2022-05-09 22:23:06 +0200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdministrationPanel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=ProductDatabase;User Id=postgres;Password=1234");
+            conn.Open();
+            NpgsqlCommand comm = new NpgsqlCommand();
+            comm.Connection = conn;
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "select * from product";
+            NpgsqlDataReader dr = comm.ExecuteReader();
+            if (dr.HasRows)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = dt;
+            }
+            dataGridView1.Columns[0].HeaderText = "ID";
+            dataGridView1.Columns[1].HeaderText = "Name";
+            dataGridView1.Columns[2].HeaderText = "Quantity";
+            dataGridView1.Columns[3].HeaderText = "Price";
+            dataGridView1.Columns[4].HeaderText = "Category";
+            comm.Dispose();
+            conn.Close();
+        }
+        private Button currentButton;
+
+        private void ActivateButton(object btnSender)
+        {
+            
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = ColorTranslator.FromHtml("#6C6EA0");
+                    currentButton.ForeColor = ColorTranslator.FromHtml("#FFFFFF");
+                    currentButton.Font = new System.Drawing.Font("Myriad Pro", 15.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panel1.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.Transparent;
+                    previousBtn.ForeColor = ColorTranslator.FromHtml("#E6FAFC");
+                    previousBtn.Font = new System.Drawing.Font("Myriad Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void shoppingButton_Click(object sender, EventArgs e)
+        {
+            sctnName.Text = shoppingButton.Text;
+            ActivateButton(sender);
+        }
+
+        private void allProductsButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void statsButton_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+    }
+}
